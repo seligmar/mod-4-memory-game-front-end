4cf23e58919d5b097fc098cb9eb0d33bd0bafe94
github username <github email address>
2019-09-30 08:56:24 +0100

    Monday morning commit

--- memory-game/src/Card.js
+++ memory-game/src/Card.js
@@ -24,14 +24,14 @@ flipCard = e => {
         <div className= "card-div">
             {this.props.removedPaintings.includes(this.props.card) ? 
             <img src="https://d32dm0rphc51dk.cloudfront.net/pdRjIGw58ecojporcDG0_w/medium.jpg" className="App-logo" alt="logo" /> : 
-                this.state.flippedOver ? 
+             this.state.flippedOver ? 
                 <div className="flipped-card-div"> 
                  <img onClick={e => (this.flipCard(e))} className = "img-flipped" src={this.props.card.image} alt={this.props.card.title} /> 
                  <p className="details">Title: {this.props.card.title} Artist: {this.props.card.artist.name}</p>
                  <button className= "question-buttons" onClick={e => this.props.checkMatch(this.props.card)}>Match?</button> 
-
-                 </div>
-                 : 
+                 </div>: 
+            //      this.props.flippedCards === 0 ? 
+            //   <div> <img onClick={e => (this.flipCardAndInitiate(e))} className = "img" src="http://www.thecontemporarychester.com/wp-content/uploads/2016/01/image9.jpg" alt="Make Art Not War artist: Shepard Fairey" /></div> :
             <div> <img onClick={e => (this.flipCardAndInitiate(e))} className = "img" src="http://www.thecontemporarychester.com/wp-content/uploads/2016/01/image9.jpg" alt="Make Art Not War artist: Shepard Fairey" /></div> } 
         </div>
         )}
